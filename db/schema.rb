bf7a79e6609102e2c8d778580ecf64ff5038699e
thescorpiolab <support@thescorpiolab.com>
2019-04-03 20:56:24 +0000

    4319 13

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_03_052411) do
+ActiveRecord::Schema.define(version: 2019_04_03_203312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "courses", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.decimal "cost"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_courses_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +38,3 @@ ActiveRecord::Schema.define(version: 2019_04_03_052411) do
   end
 
 end
-
