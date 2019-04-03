7e71ceb049e6da955f6190f12bad846545c27074
thescorpiolab <support@thescorpiolab.com>
2019-04-03 21:09:41 +0000

    4319 14

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,23 +1,26 @@
 class Instructor::CoursesController < ApplicationController
-	before_action :authenticate_user!
-	def new
-		@course = Course.new
-	end
+  before_action :authenticate_user!
 
-	def create
-		@course = current_user.courses.create(course_params)
-		redirect_to instructor_course_path(@course)
-	end
-	
-	private
+  def new
+    @course = Course.new
+  end
 
-	def course_params
-	    params.require(:course).permit(:title, :description, :cost)
-	end
+  def create
+    @course = current_user.courses.create(course_params)
+    if @course.valid?
+      redirect_to instructor_course_path(@course)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
-	def show
-		@course = Course,find(param[:id])
-	end
-		
-	end
+  def show
+    @course = Course.find(params[:id])
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:title, :description, :cost)
+  end
 end
