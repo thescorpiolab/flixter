d0859fc888806bbd6c58a92383fdc821c78d60d3
thescorpiolab <support@thescorpiolab.com>
2019-04-19 23:41:08 +0000

    41919 27

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,13 @@
-# config/initializers/carrierwave.rb
 
 CarrierWave.configure do |config|
+  if Rails.env.development?
+    config.cache_dir = '/home/vagrant/uploads_tmp/tmp/uploads'
+    config.root = '/home/vagrant/uploads_tmp/tmp'
+  end
+
   config.storage    = :aws
   config.aws_bucket = ENV["AWS_BUCKET"]
   config.aws_acl    = "public-read"
-  config.fog_public = false 
 
   config.aws_credentials = {
       access_key_id:     ENV["AWS_ACCESS_KEY"],
