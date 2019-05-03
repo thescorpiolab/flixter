e1988cf8effc3b0b7f7205e87d3cbd96d4725e5d
thescorpiolab <support@thescorpiolab.com>
2019-05-03 22:01:54 +0000

    5319 36

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -18,6 +18,7 @@ class Instructor::CoursesController < ApplicationController
   def show
     @course = Course.find(params[:id])
     @section = Section.new 
+    @lesson = Lesson.new
   end
 
   private
